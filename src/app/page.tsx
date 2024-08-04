507a770a67e72ca5aefd199e10a97eb1fd9286a6
Dominik Rycharski <dominik.rycharski@rst.com.pl>
2024-08-05 00:18:45 +0200

    feat: add initial hidden footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import { ComboboxForm } from "@/components/combobox-form";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-start p-8">
-      <ComboboxForm />
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-start p-8">
+        <ComboboxForm />
+      </main>
+      {/* <footer>
+        Created by Doryski. For any questions, bugs or feature requests, please
+        contact me through the contact form at ... or open an issue on Github
+        repository.
+      </footer> */}
+    </>
   );
 }
