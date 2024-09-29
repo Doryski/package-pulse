520690352fc5f863832b64bee08339a6d1d0f05a
Doryski <dominatorskyyy@gmail.com>
2024-09-30 01:10:28 +0200

    fix: local zone used for calculations

--- src/lib/queries/keys.ts
+++ src/lib/queries/keys.ts
@@ -1,10 +1,11 @@
 import { DATE_FORMAT } from "@/api/fetchNPMDownloads";
 import { format } from "date-fns";
+import { toZonedTime } from "date-fns-tz";
 
 export const getProjectStatsQueryKey = (projectName: string) => [
   "project-stats",
   projectName,
-  format(new Date(), DATE_FORMAT),
+  format(toZonedTime(new Date(), "UTC"), DATE_FORMAT),
 ];
 
 export const getSearchNPMRegistryQueryKey = (search: string) => [
