2224760e442d9027b16552c3dd5bcb86cd502af3
Doryski <dominatorskyyy@gmail.com>
2024-09-30 14:07:16 +0200

    feat: improve SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Package Pulse | Explore NPM Package Trends",
   description:
-    "Explore comprehensive statistics for NPM packages. View charts, tables, and subscribe for email updates on package trends and analytics.",
+    "Explore comprehensive statistics for NPM packages. View charts, tables, and subscribe for email updates on NPM trends and analytics.",
   keywords: [
     "NPM trends",
     "NPM package trends",
@@ -22,6 +22,8 @@ export const metadata = {
     "JavaScript libraries",
     "dependency trends",
     "open source analytics",
+    "Github stars",
+    "Github trends",
   ],
   openGraph: {
     title: "Package Pulse | Explore NPM Package Trends",
